5dee100fe8c7d2b0a3745b7403bd813d6102a1c7
stephenwithph <stephenwithph@users.noreply.github.com>
2020-09-07 20:04:34 -0700

    fix spacing

--- pkgs/development/python-modules/pydrive2/default.nix
+++ pkgs/development/python-modules/pydrive2/default.nix
@@ -13,7 +13,7 @@ buildPythonPackage rec {
   version = "1.6.0";
   disabled = ! isPy3k; # transitively via google_api_python_client, google_api_core no longer supports Python 2
 
-  src =fetchPypi {
+  src = fetchPypi {
     pname = "PyDrive2";
     inherit version;
     sha256 = "0000000000000000000000000000000000000000000000000000";
