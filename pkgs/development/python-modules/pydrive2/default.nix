1f7e4ec3321b71d21e3cebb3c02a5f3b2549b5b2
stephenwithph <stephenwithph@users.noreply.github.com>
2020-09-07 19:59:52 -0700

    begin work to add pydrive2

---
+++ pkgs/development/python-modules/pydrive2/default.nix
@@ -0,0 +1,34 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, isPy3k
+, google_api_python_client
+, oauth2client
+, pyyaml
+, pyopenssl
+}:
+
+buildPythonPackage rec {
+  pname = "pydrive2";
+  version = "1.6.0";
+  disabled = ! isPy3k; # transitively via google_api_python_client, google_api_core no longer supports Python 2
+
+  src =fetchPypi {
+    pname = "PyDrive2";
+    inherit version;
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+
+  propagatedBuildInputs = [
+    google_api_python_client
+    oauth2client
+    pyyaml
+    pyopenssl
+  ];
+
+  meta = {
+    description = "Google Drive API Python wrapper library";
+    homepage = "https://github.com/iterative/PyDrive2";
+    license = lib.licenses.asl20;
+  };
+}
