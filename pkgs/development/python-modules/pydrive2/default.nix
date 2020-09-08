905cb5c4b437793aea419e2f76e2c6805f0891da
stephenwithph <stephenwithph@users.noreply.github.com>
2020-09-07 20:25:40 -0700

    figure out python3 incantation... hopefully

--- pkgs/development/python-modules/pydrive2/default.nix
+++ pkgs/development/python-modules/pydrive2/default.nix
@@ -1,25 +1,16 @@
-{ lib
-, buildPythonPackage
-, fetchPypi
-, isPy3k
-, google_api_python_client
-, oauth2client
-, pyyaml
-, pyopenssl
-}:
+{ lib, python3 }:
 
-buildPythonPackage rec {
+python3.pkgs.buildPythonPackage rec {
   pname = "pydrive2";
   version = "1.6.0";
-  disabled = ! isPy3k; # transitively via google_api_python_client, google_api_core no longer supports Python 2
 
-  src = fetchPypi {
+  src = python3.pkgs.fetchPypi {
     pname = "PyDrive2";
     inherit version;
     sha256 = "0000000000000000000000000000000000000000000000000000";
   };
 
-  propagatedBuildInputs = [
+  propagatedBuildInputs = with python3.pkgs; [
     google_api_python_client
     oauth2client
     pyyaml
